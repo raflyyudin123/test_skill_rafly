bb4362507b9c7dccb782946c9dd9ba25b62a8fc3
raflyyudin123 <104190242+raflyyudin123@users.noreply.github.com>
2022-04-22 15:26:17 +0700

    Add files via upload
    source code

---
+++ raflyyudin.sql
@@ -0,0 +1,8 @@
+Select tb_sales.sales_code, tb_sales.sales_name,tb_product.product_code,tb_product.product_name,tb_transaction.total_invest,tb_transaction.count(*) as total_pembelian
+from tb_transaction join tb_sales on tb_sales.sales_code = tb_transaction.sales_code join tb_product on tb_product.product_code = tb_transaction.product_code;
+
+Select tb_product.product_code,tb_product.product_name,tb_transaction.total_invest,tb_transaction.count(*) as total_pembelian
+from tb_transaction join tb_product on tb_product.product_code = tb_transaction.product_code;
+
+Select tb_transaction.created_date tb_product.product_code,tb_product.product_name,tb_transaction.total_invest,tb_transaction.count(*) as total_pembelian
+from tb_transaction join tb_product on tb_product.product_code = tb_transaction.product_code;
